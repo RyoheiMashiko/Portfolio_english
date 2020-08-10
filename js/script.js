58e5ced413f456f061fc4efe9709e3fd676e3ca7
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-08-10 09:37:15 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 const navOverlay = document.getElementById("nav");
 const humburger = document.querySelector(".humburger");
-const closeButton = document.querySelector(".close");
+const closeButton = document.querySelector(".close-button");
 
 humburger.addEventListener("click", () => {
   navOverlay.classList.remove("invisible");
+  navOverlay.classList.remove("fade-out");
+  navOverlay.classList.add("fade-in");
+  humburger.classList.add("invisible");
 } );
 
 closeButton.addEventListener("click", () => {
-  navOverlay.classList.add("invisible");
+  navOverlay.classList.add("fade-out");
+  humburger.classList.remove("invisible");
+  const delay= function() {navOverlay.classList.add("invisible")};
+  setTimeout(delay,800);
+  navOverlay.classList.remove("fade-in");
 } );
