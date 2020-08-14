8d07e1b120285b1cc4602447d225a89d78b74e17
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-08-14 16:07:28 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
 const navOverlay = document.getElementById("nav");
-const humburger = document.querySelector(".humburger");
+const menuBtn = document.querySelector(".menu-btn");
 const closeButton = document.querySelector(".close");
+let menuOpen = false;
 
-humburger.addEventListener("click", () => {
-  navOverlay.classList.remove("invisible");
-  navOverlay.classList.remove("fade-out");
-  navOverlay.classList.add("fade-in");
-  humburger.classList.add("invisible");
-} );
+menuBtn.addEventListener("click", () => {
+  if(!menuOpen) {
+    menuBtn.classList.add("open");
+    navOverlay.classList.remove("invisible");
+    navOverlay.classList.remove("fade-out");
+    navOverlay.classList.add("fade-in");
+    menuOpen= true;
+  }else {
+    menuBtn.classList.remove("invisible");
+    navOverlay.classList.add("fade-out");
+    menuBtn.classList.remove("open");
+    menuOpen= false;
+    const delay= function() {navOverlay.classList.add("invisible")};
+    setTimeout(delay,400);
+    navOverlay.classList.remove("fade-in");
+  }
+});
 
-closeButton.addEventListener("click", () => {
-  humburger.classList.remove("invisible");
-  navOverlay.classList.add("fade-out");
-  const delay= function() {navOverlay.classList.add("invisible")};
-  setTimeout(delay,400);
-  navOverlay.classList.remove("fade-in");
-} );
+// menuBtn.addEventListener("click", () => {
+//   navOverlay.classList.remove("invisible");
+//   navOverlay.classList.remove("fade-out");
+//   navOverlay.classList.add("fade-in");
+//   menuBtn.classList.add("invisible");
+// } );
+
+// closeButton.addEventListener("click", () => {
+//   menuBtn.classList.remove("invisible");
+//   navOverlay.classList.add("fade-out");
+//   const delay= function() {navOverlay.classList.add("invisible")};
+//   setTimeout(delay,400);
+//   navOverlay.classList.remove("fade-in");
+// } );
