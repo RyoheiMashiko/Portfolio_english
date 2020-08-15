1169d8f40c9f596e18330af4d1512886271517b5
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-08-15 16:27:16 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@ const navOverlay = document.getElementById("nav");
 const menuBtn = document.querySelector(".menu-btn");
 const menuBtnBurger = document.querySelector(".menu-btn-burger");
 const closeButton = document.querySelector(".close");
+const menuBtnBurgerTop = document.querySelector(".menu-btn-burger2");
+const menuBtnBurgerBottom = document.querySelector(".menu-btn-burger3");
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
   if(!menuOpen) {
     menuBtn.classList.add("open");
     menuBtnBurger.classList.add("bg-white");
+    menuBtnBurgerTop.classList.add("bg-white");
+    menuBtnBurgerBottom.classList.add("bg-white");
     navOverlay.classList.remove("invisible");
     navOverlay.classList.remove("fade-out");
     navOverlay.classList.add("fade-in");
@@ -18,6 +22,8 @@ menuBtn.addEventListener("click", () => {
     navOverlay.classList.add("fade-out");
     menuBtn.classList.remove("open");
     menuBtnBurger.classList.remove("bg-white");
+    menuBtnBurgerTop.classList.remove("bg-white");
+    menuBtnBurgerBottom.classList.remove("bg-white");
     menuOpen= false;
     const delay= function() {navOverlay.classList.add("invisible")};
     setTimeout(delay,400);
@@ -42,6 +48,36 @@ ScrollReveal().reveal(".scroll", {
 });
 
 //Changing color of hamburger menu icon by background-color
-function burgerColor() {
-  
+function Color() {
+
+  let trigger = document.getElementById('About');
+  let triggerY= trigger.getBoundingClientRect().top;
+  console.log(triggerY);
+ 
+ if ( triggerY <= 15) {
+    menuBtnBurgerTop.classList.add("bg-black");
+    menuBtnBurger.classList.add("bg-black");
+    menuBtnBurgerBottom.classList.add("bg-black");
+ } else if (triggerY <= 16 ) {
+   menuBtnBurgerTop.classList.add("bg-black");
+ } else if ( triggerY <= 26) {
+   menuBtnBurger.classList.add("bg-black");
+   menuBtnBurgerTop.classList.remove("bg-black");
+ } else if (triggerY <= 35) {
+  menuBtnBurgerBottom.classList.add("bg-black");
+  menuBtnBurger.classList.remove("bg-black");
+ } else if ( triggerY >= 36 ) {
+  menuBtnBurger.classList.remove("bg-black");
+  menuBtnBurgerTop.classList.remove("bg-black");
+  menuBtnBurgerBottom.classList.remove("bg-black");
 }
+
+const  navHome= document.querySelector("nav-home");
+const  navAbout= document.querySelector("nav-about");
+const  navProject= document.querySelector("nav-project");
+const  navContect= document.querySelector("nav-contact");
+
+if ( triggerY )
+} 
+
+window.addEventListener('scroll', Color);
