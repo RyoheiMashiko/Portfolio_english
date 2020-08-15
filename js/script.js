7316d13fe8d9d31828ecb4f843c2194f6b9dda06
RyoheiMashiko <RyoheiMashiko@users.noreply.github.com>
2020-08-15 13:45:46 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+//Function of appearing overlapped nav when humburger icon clicked 
 const navOverlay = document.getElementById("nav");
 const menuBtn = document.querySelector(".menu-btn");
+const menuBtnBurger = document.querySelector(".menu-btn-burger");
 const closeButton = document.querySelector(".close");
 let menuOpen = false;
 
 menuBtn.addEventListener("click", () => {
   if(!menuOpen) {
     menuBtn.classList.add("open");
+    menuBtnBurger.classList.add("bg-white");
     navOverlay.classList.remove("invisible");
     navOverlay.classList.remove("fade-out");
     navOverlay.classList.add("fade-in");
@@ -14,6 +17,7 @@ menuBtn.addEventListener("click", () => {
     // menuBtn.classList.remove("invisible");
     navOverlay.classList.add("fade-out");
     menuBtn.classList.remove("open");
+    menuBtnBurger.classList.remove("bg-white");
     menuOpen= false;
     const delay= function() {navOverlay.classList.add("invisible")};
     setTimeout(delay,400);
@@ -32,8 +36,12 @@ navText.addEventListener("click", () => {
   navOverlay.classList.remove("fade-in");
 })
 
-
-
+// Adding an animation of floating up each element
 ScrollReveal().reveal(".scroll", {
   reset: true
 });
+
+//Changing color of hamburger menu icon by background-color
+function burgerColor() {
+  
+}
